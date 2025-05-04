43f3dbce5e34d586422927170766f3c6b3b182de
Patryk Pochmara <patonymous@gmail.com>
2025-05-04 19:15:51 +0200

    Adapt sample Qt+OCCT into this project

--- app/main.cpp
+++ app/main.cpp
@@ -3,6 +3,11 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+#if defined(_WIN32)
+    // never use ANGLE on Windows, since OCCT 3D Viewer does not expect this
+    QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+    // QCoreApplication::setAttribute (Qt::AA_UseOpenGLES);
+#endif
     QApplication a(argc, argv);
     MainWindow   w;
     w.show();
--- app/occtwidget/occtwidget.h
+++ app/occtwidget/occtwidget.h
@@ -1,3 +1,6 @@
+// Adapted from
+// https://github.com/gkv311/occt-samples-qopenglwidget/blob/master/occt-qopenglwidget/OcctQtViewer.h
+
 #ifndef OCCTWIDGET_H
 #define OCCTWIDGET_H
 
@@ -5,22 +8,80 @@
 #include <QOpenGLFunctions>
 #include <QOpenGLWidget>
 
-class OcctWidget : public QOpenGLWidget, QOpenGLFunctions {
+#include <AIS_InteractiveContext.hxx>
+#include <AIS_ViewController.hxx>
+#include <AIS_ViewCube.hxx>
+#include <V3d_View.hxx>
+
+class OcctWidget : public QOpenGLWidget,
+                   QOpenGLFunctions,
+                   public AIS_ViewController {
     Q_OBJECT
 
 public:
     OcctWidget(QWidget *parent = nullptr);
     ~OcctWidget();
 
+    // Return Viewer.
+    const Handle(V3d_Viewer) & Viewer() const { return m_viewer; }
+
+    // Return View.
+    const Handle(V3d_View) & View() const { return m_view; }
+
+    // Return AIS context.
+    const Handle(AIS_InteractiveContext) & Context() const { return m_context; }
+
+    // Return OpenGL info.
+    const QString &getGlInfo() const { return m_glInfo; }
+
+    // Minial widget size.
+    virtual QSize minimumSizeHint() const override { return QSize(200, 200); }
+
+    // Default widget size.
+    virtual QSize sizeHint() const override { return QSize(720, 480); }
+
+    // Handle subview focus change.
+    virtual void OnSubviewChanged(
+        const Handle(AIS_InteractiveContext) &, const Handle(V3d_View) &,
+        const Handle(V3d_View) & theNewView
+    ) override;
+
 protected:
     void initializeGL() override;
     void resizeGL(int width, int height) override;
     void paintGL() override;
 
-private:
-    QOpenGLDebugLogger m_logger;
+    void closeEvent(QCloseEvent *theEvent) override;
+    void keyPressEvent(QKeyEvent *theEvent) override;
+    void mousePressEvent(QMouseEvent *theEvent) override;
+    void mouseReleaseEvent(QMouseEvent *theEvent) override;
+    void mouseMoveEvent(QMouseEvent *theEvent) override;
+    void wheelEvent(QWheelEvent *theEvent) override;
 
+private:
+    // Print debug messages.
     void printDebugMessages() const;
+
+    // Dump OpenGL info.
+    void dumpGlInfo(bool theIsBasic, bool theToPrint);
+
+    // Handle view redraw.
+    virtual void handleViewRedraw(
+        const Handle(AIS_InteractiveContext) & theCtx,
+        const Handle(V3d_View) & theView
+    ) override;
+
+    // clang-format off
+    Handle(V3d_Viewer)             m_viewer;
+    Handle(V3d_View)               m_view;
+    Handle(V3d_View)               m_focusView;
+    Handle(AIS_InteractiveContext) m_context;
+    Handle(AIS_ViewCube)           m_viewCube;
+    // clang-format on
+
+    QString            m_glInfo;
+    bool               m_isCoreProfile;
+    QOpenGLDebugLogger m_logger;
 };
 
 #endif // OCCTWIDGET_H
